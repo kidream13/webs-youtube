01a8c257fc5d9b231e8f32536bfe1ddf34f6ab93
kidream13 <you@example.com>
2025-03-12 15:26:52 +0900

    섹션 콤포넌트 구조화 하기

--- src/App.js
+++ src/App.js
@@ -14,10 +14,15 @@ import Channel from './pages/Channel'
 import Video from './pages/Video'
 import Search from './pages/Search'
 import Not from './pages/Not'
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
+    <Main>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/today' element={<Today/>}/>
@@ -32,6 +37,8 @@ const App = () => {
         <Route path='/channel/:searchID' element={<Search/>}/>
         <Route path='/*' element={<Not/>}/>
       </Routes>
+    </Main>
+    <Footer/>
     </BrowserRouter>
   )
 }
